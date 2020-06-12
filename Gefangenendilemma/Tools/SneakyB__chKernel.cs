5100eabff37e271bc915894332f2b0a3b17399d3
FireRune <46537748+FireRune@users.noreply.github.com>
2020-06-12 19:33:44 +0200

    Implementation Aufgabe T1

---
+++ Gefangenendilemma/Tools/SneakyB__chKernel.cs
@@ -0,0 +1,140 @@
+﻿using Gefangenendilemma.Basis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gefangenendilemma.Tools
+{
+    class SneakyB__chKernel
+    {
+        private int _decision;
+        private int _lastDecision;
+        private int _round;
+        private int _weight;
+        private int _mood;
+        private int _moodCountPositive;
+        private int _moodCountNegative;
+
+
+        public SneakyB__chKernel()
+        {
+            this._decision = 0;
+            this._mood = 0;
+            this._moodCountPositive = 0;
+            this._moodCountNegative = 0;
+        }
+
+
+        public int GetResult()
+        {
+            Thinking();
+            return _decision;
+        }
+
+        public void StartUp(int round, int weight)
+        {
+            this._round = round;
+            this._weight = weight;
+
+            if (_moodCountNegative < 3 && _moodCountPositive > 2)
+            {
+                _mood = 0;
+                _moodCountNegative = 0;
+                _moodCountPositive = 0;
+            }
+            if (_lastDecision == BasisStrategie.Verrat)
+            {
+                _moodCountNegative += 1;
+                _moodCountPositive -= 1;
+            }
+            if (_lastDecision == BasisStrategie.Kooperieren)
+            {
+                _moodCountPositive += 1;
+                _moodCountNegative -= 1;
+            }
+        }
+
+        private int Betray()
+        {
+            _mood += 2;
+            return BasisStrategie.Verrat;
+        }
+
+        private int Cooperate()
+        {
+            return BasisStrategie.Kooperieren;
+        }
+
+        public void SetLastDecision(int lastDecision)
+        {
+            this._lastDecision = lastDecision;
+        }
+
+        private void CheckForTraitor()
+        {
+            if (_lastDecision == BasisStrategie.Verrat)
+            {
+                _mood += 1;
+            }
+        }
+
+        private void Thinking()
+        {
+            CheckForTraitor();
+            switch (_weight)
+            {
+                case 0:
+                    if (_round == 0)
+                    {
+                        _decision = Cooperate();
+                    }
+                    else
+                    {
+                        if (_moodCountPositive > _moodCountNegative)
+                        {
+                            _decision = Betray();
+                        }
+                        else
+                        {
+                            _decision = Cooperate();
+                        }
+                    }
+                    break;
+                case 1:
+                    if (_round == 0)
+                    {
+                        _decision = Cooperate();
+                    }
+                    else
+                    {
+                        if (_moodCountPositive < _moodCountNegative && _mood > 4)
+                        {
+                            _decision = Betray();
+                        }
+                        else
+                        {
+                            _decision = Cooperate();
+                        }
+                    }
+                    break;
+                case 2:
+                    if (_round == 0)
+                    {
+                        _decision = Cooperate();
+                    }
+                    else
+                    {
+                        if (_moodCountPositive > _moodCountNegative)
+                        {
+                            _decision = Betray();
+                        }
+                        else
+                        {
+                            _decision = Cooperate();
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
--- Gefangenendilemma/VerwaltungProgramm.cs
+++ Gefangenendilemma/VerwaltungProgramm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using Gefangenendilemma.Basis;
 
 namespace Gefangenendilemma
@@ -13,7 +14,7 @@ namespace Gefangenendilemma
         /// Diese Liste(Collection) enthält alle Gefangene/Strategien
         /// </summary>
         private static List<BasisStrategie> _strategien;
-        
+
         static void Main(string[] args)
         {
             //bekannt machen der ganzen strategien
@@ -22,17 +23,19 @@ namespace Gefangenendilemma
             _strategien.Add(new VerrateImmerStrategie());
             _strategien.Add(new Strategie_Jonas_Pfalzgraf());
             _strategien.Add(new Strategie_Max_Matthiolius());
-            _strategien.Add(new Strategie3());
-            
+            _strategien.Add(new Strategie_Fabian_Doepke());
+          
             string eingabe;
             do
             {
                 // Begrüßung
                 Console.WriteLine("Willkommen zum Gefangenendilemma");
                 Console.WriteLine("0 - Verhör zwischen 2 Strategien");
-                Console.WriteLine("2 - Verhör zwischen Benutzer und Strategie");
+                Console.WriteLine("1 - Verhör zwischen Benutzer und Strategie");
+                Console.WriteLine("2 - 2 Strategien die 9 Spiele gegenander spielen und erst dann wird der Sieger gekürt");
+                Console.WriteLine("3 - Turnier zwischen den 3 Strategien des Teams");
                 Console.WriteLine("X - Beenden");
-                
+
                 // Eingabe
                 Console.Write("Treffen Sie ihre Option: ");
                 eingabe = Console.ReadLine();
@@ -43,8 +46,14 @@ namespace Gefangenendilemma
                     case "0":
                         Gefangene2();
                         break;
-                    case "2":
+                    case "1":
                         ManAgainstMachine();
+                        break;
+                    case "2":
+                        BestOfNine();
+                        break;
+                    case "3":
+                        TournamentMode();
                         break;
                     case "X":
                         break;
@@ -62,7 +71,7 @@ namespace Gefangenendilemma
         {
             int st1, st2;
             int runde, schwere;
-            
+
             Console.WriteLine("Willkommen zum Verhör zwischen 2 Strategien");
             for (int i = 0; i < _strategien.Count; i++)
             {
@@ -72,8 +81,8 @@ namespace Gefangenendilemma
             st1 = VerwaltungKram.EingabeZahlMinMax("Wählen Sie die 1. Strategie", 0, _strategien.Count);
             st2 = VerwaltungKram.EingabeZahlMinMax("Wählen Sie die 2. Strategie", 0, _strategien.Count);
             runde = VerwaltungKram.EingabeZahlMinMax("Wie viele Runden sollen diese verhört werden?", 1, 101);
-            schwere = VerwaltungKram.EingabeZahlMinMax("Wie schwer sind die Verstöße? (0=leicht, 1=mittel, 2=schwer)", -1, 3);
-            
+            schwere = VerwaltungKram.EingabeZahlMinMax("Wie schwer sind die Verstöße? (0=leicht, 1=mittel, 2=schwer)", 0, 3);
+
             Verhoer(st1, st2, runde, schwere);
         }
 
@@ -91,7 +100,189 @@ namespace Gefangenendilemma
             runde = VerwaltungKram.EingabeZahlMinMax("Wie viele Runden sollen diese verhört werden?", 1, 101);
             schwere = VerwaltungKram.EingabeZahlMinMax("Wie schwer sind die Verstöße? (0=leicht, 1=mittel, 2=schwer)", 0, 3);
 
-            Verhoer(st, runde, schwere);
+            VerhoerMitUser(st, runde, schwere);
+        }
+
+        static void TournamentMode()
+        {
+            int[] punkte = new int[] { 0, 0, 0 };
+            int st1 = 2; //_strategien[2]
+            int st2 = 3; //_strategien[3]
+            int st3 = 4; //_strategien[4]
+            int[] punkte1u2;
+            int[] punkte1u3;
+            int[] punkte2u3;
+            for(int i = 0; i < 3; i++)
+            {
+                if (i == 0)
+                {
+                    punkte1u2 = GetPunkteFromVerhoer(st1, st2, 5, i);
+                    punkte[0] += punkte1u2[0] * 20;
+                    punkte[1] += punkte1u2[1] * 20;
+                }
+                else if (i == 1)
+                {
+                    punkte1u2 = GetPunkteFromVerhoer(st1, st2, 25, i);
+                    punkte[0] = punkte1u2[0] * 4;
+                    punkte[1] = punkte1u2[1] * 4;
+                }
+                else
+                {
+                    punkte1u2 = GetPunkteFromVerhoer(st1, st2, 100, i);
+                    punkte[0] = punkte1u2[0];
+                    punkte[1] = punkte1u2[1];
+                }
+            }
+            for(int i = 0; i < 3; i++)
+            {
+                if (i == 0)
+                {
+                    punkte1u3 = GetPunkteFromVerhoer(st1, st3, 5, i);
+                    punkte[0] = punkte1u3[0] * 20;
+                    punkte[2] = punkte1u3[1] * 20;
+                }
+                else if (i == 1)
+                {
+                    punkte1u3 = GetPunkteFromVerhoer(st1, st3, 25, i);
+                    punkte[0] = punkte1u3[0] * 4;
+                    punkte[2] = punkte1u3[1] * 4;
+                }
+                else
+                {
+                    punkte1u3 = GetPunkteFromVerhoer(st1, st3, 100, i);
+                    punkte[0] = punkte1u3[0];
+                    punkte[2] = punkte1u3[1];
+                }
+            }
+            for(int i = 0; i < 3; i++)
+            {
+                if (i == 0)
+                {
+                    punkte2u3 = GetPunkteFromVerhoer(st2, st3, 5, i);
+                    punkte[1] = punkte2u3[0] * 20;
+                    punkte[2] = punkte2u3[1] * 20;
+                }
+                else if (i == 1)
+                {
+                    punkte2u3 = GetPunkteFromVerhoer(st2, st3, 25, i);
+                    punkte[1] = punkte2u3[0] * 4;
+                    punkte[2] = punkte2u3[1] * 4;
+                }
+                else
+                {
+                    punkte2u3 = GetPunkteFromVerhoer(st2, st3, 100, i);
+                    punkte[1] = punkte2u3[0];
+                    punkte[2] = punkte2u3[1];
+                }
+            }
+            ErmittleSieger(punkte);
+        }
+        private static void ErmittleSieger(int[] punkte)
+        {
+            int pktST1 = punkte[0]; int pktST2 = punkte[1]; int pktST3 = punkte[2];
+            //ermittle ob alle Strategien die gleiche Punktzahl haben
+            if (pktST1 == pktST2 && pktST1 == pktST3)
+            {
+                Console.WriteLine($"Alle 3 Strategien des Teams haben die gleiche Punktzahl von {pktST1} erreicht.");
+            }
+            else
+            {
+                int platzST1 = 0; int platzST2 = 0; int platzST3 = 0;
+                //ermittle Platzierung von Team-Strategie 1
+                if (pktST1 < pktST2 && pktST1 < pktST3 && pktST2 != pktST3)
+                    platzST1 = 3;
+                else if (pktST1 == pktST2 || pktST1 == pktST3)
+                    platzST1 = 2;
+                else if ((pktST1 < pktST2 && pktST1 > pktST3) || (pktST1 > pktST2 && pktST1 < pktST3))
+                    platzST1 = 2;
+                else if (pktST2 == pktST3 && pktST2 > pktST1)
+                    platzST1 = 2;
+                else if (pktST1 >= pktST2 && pktST1 >= pktST3)
+                    platzST1 = 1;
+                //ermittle Platzierung von Team-Strategie 2 und 3
+                if (platzST1 == 1)
+                {
+                    if (pktST2 == pktST1 && pktST2 > pktST3)
+                    {
+                        platzST2 = 1; platzST3 = 2;
+                    }
+                    else if (pktST3 == pktST1 && pktST3 > pktST2)
+                    {
+                        platzST2 = 2; platzST3 = 1;
+                    }
+                    else if (pktST2 == pktST3)
+                    {
+                        platzST2 = 2; platzST3 = 2;
+                    }
+                    else if (pktST2 < pktST1 && pktST2 > pktST3)
+                    {
+                        platzST2 = 2; platzST3 = 3;
+                    }
+                    else
+                    {
+                        platzST2 = 3; platzST3 = 2;
+                    }
+                }
+                else if (platzST1 == 2)
+                {
+                    if (pktST2 > pktST1 && pktST2 == pktST3)
+                    {
+                        platzST2 = 1; platzST3 = 1;
+                    }
+                    else if (pktST2 > pktST1 && pktST1 == pktST3)
+                    {
+                        platzST2 = 1; platzST3 = 2;
+                    }
+                    else if (pktST3 > pktST1 && pktST1 == pktST2)
+                    {
+                        platzST2 = 2; platzST3 = 1;
+                    }
+                    else if (pktST2 > pktST1 && pktST3 < pktST1)
+                    {
+                        platzST2 = 1; platzST3 = 3;
+                    }
+                    else
+                    {
+                        platzST2 = 3; platzST3 = 1;
+                    }
+                }
+                else
+                {
+                    if (pktST2 > pktST3)
+                    {
+                        platzST2 = 1; platzST3 = 2;
+                    }
+                    else if (platzST2 < platzST3)
+                    {
+                        platzST2 = 2; platzST3 = 1;
+                    }
+                }
+                Console.WriteLine($"Team-Strategie 1 belegt Platz {platzST1} mit {pktST1} Punkten.");
+                Console.WriteLine($"Team-Strategie 2 belegt Platz {platzST2} mit {pktST2} Punkten.");
+                Console.WriteLine($"Team-Strategie 3 belegt Platz {platzST3} mit {pktST3} Punkten.");
+            }
+        }
+
+        /// <summary>
+        /// Fragt 2 Strategien und Schwere ab und spielt ein best of nine.
+        /// </summary>
+        static void BestOfNine()
+        {
+            int st1, st2;
+            int schwere;
+
+            Console.WriteLine("Willkommen zum Best of 9 zwischen 2 Strategien");
+            for (int i = 0; i < _strategien.Count; i++)
+            {
+                Console.WriteLine($"{i} - {_strategien[i].Name()}");
+            }
+
+            Console.WriteLine("Wählen Sie ihre 2 Strategien:");
+            st1 = VerwaltungKram.EingabeZahlMinMax("Wählen Sie die 1. Strategie", 0, _strategien.Count);
+            st2 = VerwaltungKram.EingabeZahlMinMax("Wählen Sie die 2. Strategie", 0, _strategien.Count);
+            schwere = VerwaltungKram.EingabeZahlMinMax("Wie schwer sind die Verstöße? (0=leicht, 1=mittel, 2=schwer)", 0, 3);
+
+            Verhoer(st1, st2, 9, schwere);
         }
 
         /// <summary>
@@ -115,15 +306,16 @@ namespace Gefangenendilemma
             //beide Strategien über den Start informieren (Also es wird die Startmethode aufgerufen)
             strategie1.Start(runde, schwere);
             strategie2.Start(runde, schwere);
-            
+
             Console.WriteLine($"Verhör zwischen {strategie1.Name()} und {strategie2.Name()} für {runde} Runden.");
-            
+
             //start
             for (int i = 0; i < runde; i++)
             {
                 //beide verhören
                 int aktReaktion1 = strategie1.Verhoer(reaktion2);
                 int aktReaktion2 = strategie2.Verhoer(reaktion1);
+                // verhoerWahrsch(aktReaktion1, aktReaktion2, strategie1.Name(), strategie2.Name());
 
                 //punkte berechnen                
                 switch (schwere)
@@ -145,27 +337,28 @@ namespace Gefangenendilemma
                 reaktion1 = aktReaktion1;
                 reaktion2 = aktReaktion2;
             }
-            
+
             //ausgabe
             Console.WriteLine($"{strategie1.Name()} hat {punkte1} Punkte erhalten.");
             Console.WriteLine($"{strategie2.Name()} hat {punkte2} Punkte erhalten.");
             if (punkte1 < punkte2)
             {
                 Console.WriteLine("Somit hat {0} gewonnen.", strategie1.Name());
-            } 
+            }
             else
             {
                 Console.WriteLine("Somit hat {0} gewonnen.", strategie2.Name());
             }
-            
+
         }
+
         /// <summary>
         /// Startet ein Verhör zwischen der Strategie an der Position st und dem Benutzer über die Länge von runde und der Schwere schwere
         /// </summary>
         /// <param name="st"></param>
         /// <param name="runde"></param>
         /// <param name="schwere"></param>
-        static void Verhoer(int st, int runde, int schwere)
+        static void VerhoerMitUser(int st, int runde, int schwere)
         {
             //holt die Strategie aus der Collection.
             BasisStrategie strategie = _strategien[st];
@@ -238,6 +431,52 @@ namespace Gefangenendilemma
         }
 
         /// <summary>
+        /// Startet ein Verhör zwischen der Strategie an der Position st1 und Position st2 über die Länge von runde und der Schwere schwere und gibt ein int-Array mit den Punkten der Strategien zurück
+        /// </summary>
+        /// <param name="st1"></param>
+        /// <param name="st2"></param>
+        /// <param name="runde"></param>
+        /// <param name="schwere"></param>
+        static int[] GetPunkteFromVerhoer(int st1, int st2, int runde, int schwere)
+        {
+            //holt die beiden Strategien aus der Collection.
+            BasisStrategie strategie1 = _strategien[st1];
+            BasisStrategie strategie2 = _strategien[st2];
+            //setzt Startwerte
+            int reaktion1 = BasisStrategie.NochNichtVerhoert;
+            int reaktion2 = BasisStrategie.NochNichtVerhoert;
+            int punkte1 = 0, punkte2 = 0;
+            //beide Strategien über den Start informieren (Also es wird die Startmethode aufgerufen)
+            strategie1.Start(runde, schwere);
+            strategie2.Start(runde, schwere);
+            Console.WriteLine($"Verhör zwischen {strategie1.Name()} und {strategie2.Name()} für {runde} Runden.");
+            //start
+            for (int i = 0; i < runde; i++)
+            {
+                //beide verhören
+                int aktReaktion1 = strategie1.Verhoer(reaktion2);
+                int aktReaktion2 = strategie2.Verhoer(reaktion1);
+                //punkte berechnen                
+                switch (schwere)
+                {
+                    case 0:
+                        VerhoerLeichtPunkte(aktReaktion1, aktReaktion2, ref punkte1, ref punkte2);
+                        break;
+                    case 1:
+                        VerhoerMittelPunkte(aktReaktion1, aktReaktion2, ref punkte1, ref punkte2);
+                        break;
+                    case 2:
+                        VerhoerSchwerPunkte(aktReaktion1, aktReaktion2, ref punkte1, ref punkte2);
+                        break;
+                }
+                //reaktion für den nächsten durchlauf merken
+                reaktion1 = aktReaktion1;
+                reaktion2 = aktReaktion2;
+            }
+            return new int[] { punkte1, punkte2 };
+        }
+
+        /// <summary>
         /// Berechnet für schwere Verstöße die Punkte und verwendet die 2 letzten Eingabeparameter als Rückgabe
         /// </summary>
         /// <param name="aktReaktion1"></param>
@@ -251,7 +490,7 @@ namespace Gefangenendilemma
                 punkte1 += 4;
                 punkte2 += 4;
                 return;
-            } 
+            }
             if (aktReaktion1 == BasisStrategie.Verrat && aktReaktion2 == BasisStrategie.Kooperieren)
             {
                 punkte1 += 0;
@@ -264,10 +503,10 @@ namespace Gefangenendilemma
                 punkte2 += 0;
                 return;
             }
-            
+
             punkte1 += 8;
             punkte2 += 8;
-            
+
         }
 
         /// <summary>
@@ -333,6 +572,59 @@ namespace Gefangenendilemma
 
             punkte1 += 6;
             punkte2 += 6;
+
+        }
+
+        /// <summary>
+        /// Berechnet den Zeitpunkt, ab welchem eine Strategie zuverlässig gewinnt.
+        /// </summary>
+        /// <param name="aktReaktion1"></param>
+        /// <param name="aktReaktion2"></param>
+        /// <param name="name1"></param>
+        /// <param name="name2"></param>
+        static void verhoerWahrsch(int aktReaktion1, int aktReaktion2, string name1, string name2)
+        {
+            int runde = 0, draw = 0;
+            int s1 = 0, s2 = 0;
+            float prozentS1, prozentS2;
+            bool auswS1, auswS2;
+
+            do
+            {
+                runde += 1;
+                if (aktReaktion1 == BasisStrategie.Kooperieren && aktReaktion2 == BasisStrategie.Kooperieren)
+                {
+                    draw += 1;
+
+                }
+
+                if (aktReaktion1 == BasisStrategie.Verrat && aktReaktion2 == BasisStrategie.Kooperieren)
+                {
+                    s1 += 1;
+                }
+
+                if (aktReaktion1 == BasisStrategie.Kooperieren && aktReaktion2 == BasisStrategie.Verrat)
+                {
+                    s2 += 1;
+
+                }
+
+                prozentS1 = s1 / runde - draw;
+                prozentS2 = s2 / runde - draw;
+
+                auswS1 = (prozentS1 > 0.50);
+                auswS2 = (prozentS2 > 0.50);
+
+            } while (runde > 4 && auswS1 || auswS2);
+
+            if (auswS1)
+            {
+                Console.WriteLine("{0} gewinnt in der {1} Runde.", runde, name1);
+            }
+            else if (auswS2)
+            {
+                Console.WriteLine("{0} gewinnt in der {1} Runde.", runde, name2);
+            }
 
         }
     }
